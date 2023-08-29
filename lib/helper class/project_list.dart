6aebc33d7a62761098b7dcc89233f50872fcedba
Rafid <rafidtawhid@gmail.com>
2023-08-29 18:46:51 +0600

    project details added

---
+++ lib/helper class/project_list.dart
@@ -0,0 +1,8 @@
+class ProjectList{
+  String image;
+  String title;
+  String description;
+  String tools;
+
+  ProjectList({required this.image,required this.title,required this.description,required this.tools});
+}
--- lib/views/my_portfolio.dart
+++ lib/views/my_portfolio.dart
@@ -1,15 +1,12 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+
 import '../globals/app_assets.dart';
-import '../globals/app_buttons.dart';
 import '../globals/app_colors.dart';
 import '../globals/app_text_styles.dart';
 import '../globals/constants.dart';
 import '../helper class/helper_class.dart';
-import '../widgets/profile_animation.dart';
-import '../globals/app_colors.dart';
-import '../globals/app_text_styles.dart';
-import '../globals/constants.dart';
+import '../helper class/project_list.dart';
 
 class MyPortfolio extends StatefulWidget {
   const MyPortfolio({Key? key}) : super(key: key);
@@ -28,6 +25,16 @@ class _MyPortfolioState extends State<MyPortfolio> {
     AppAssets.work2,
     AppAssets.work1,
     AppAssets.work2,
+  ];
+
+  List<ProjectList> projectList=[
+    ProjectList(image: AppAssets.work1, title: 'Remit Money Transfer',description: 'Money Transfer app for Australia to any country with green id secured verification and other latest functionalities', tools: '[Rest-Api , Google Map location, Secured bearer token,Bio metric verification, Smart UI]'),
+    ProjectList(image: AppAssets.work2, title: 'GLO Ticket',description: 'Lottery app to buy, sell and exchange for Malaysian local people', tools: '[Rest-Api, Secured authentication, Awesome animated UI]'),
+    ProjectList(image: AppAssets.work1, title: 'Pencilbox Training Institute',description: 'Converting full Functional website through Restfull api With Integrated Payment System and Live functionalities.', tools: '[Rest-Api,Provider,Payment,AamarPay,Bkash,Live Data,Functional Menus,Push Notification]'),
+    ProjectList(image: AppAssets.work2, title: 'TexPart Contact App',description: 'Project for Texpart\'s Employee where admin can filter , contact and transfer employees with all log\'s history, ML kit Contact Registration.', tools: '[Firebase backend,Various-Filtering,Admin Panel,call-sms-mail System,ML kit Camera Scanning]'),
+    ProjectList(image: AppAssets.work1, title: 'Ecomrs App',description: 'User can chose,search,details & buy any product from a smart UI and add to cart his chosen product. Admin can CRUD products and have a panel of controling the orders and payment', tools: '[FIREBASE , RECYCLERVIEW , SEARCHVIEW , PICASSO, NAVIGATION,TOOLBAR,ADMIN PANEL,CUSTOMER PANEL]'),
+    ProjectList(image: AppAssets.work1, title: 'Music Player [Java]',description: 'show all music file and play,pause,running in background,fast-forward with smart UI,Theme Change,Toolbar.', tools: '[ System Services,Custom seekbar,Visualizer,Background services ]'),
+
   ];
 
   var hoveredIndex;
@@ -67,17 +74,17 @@ class _MyPortfolioState extends State<MyPortfolio> {
 
   GridView buildProjectGridView({required int crossAxisCount}) {
     return GridView.builder(
-      itemCount: images.length,
+      itemCount: projectList.length,
       shrinkWrap: true,
       physics: const NeverScrollableScrollPhysics(),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: crossAxisCount,
-        mainAxisExtent: 280,
+        mainAxisExtent: 300,
         mainAxisSpacing: 24,
         crossAxisSpacing: 24,
       ),
       itemBuilder: (context, index) {
-        var image = images[index];
+        var project = projectList[index];
         return FadeInUpBig(
           duration: const Duration(milliseconds: 1600),
           child: InkWell(
@@ -95,10 +102,11 @@ class _MyPortfolioState extends State<MyPortfolio> {
               alignment: Alignment.center,
               children: [
                 Container(
+
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(20),
                     image: DecorationImage(
-                        image: AssetImage(image), fit: BoxFit.fill),
+                        image: AssetImage(project.image,), fit: BoxFit.fill),
                   ),
                 ),
                 Visibility(
@@ -123,33 +131,43 @@ class _MyPortfolioState extends State<MyPortfolio> {
                     ),
                     child: Column(
                       children: [
-                        Text(
-                          'App Development',
+                        Text(project.title,
                           style: AppTextStyles.montserratStyle(
                               color: Colors.black87, fontSize: 20),
                         ),
                         Constants.sizedBox(height: 15.0),
-                        Text(
-                          'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+                        Text(project.description,
                           style:
-                              AppTextStyles.normalStyle(color: Colors.black87),
+                          AppTextStyles.normalStyle(color: Colors.black87),
                           textAlign: TextAlign.center,
                         ),
-                        Constants.sizedBox(height: 30.0),
-                        CircleAvatar(
-                          maxRadius: 25,
-                          backgroundColor: Colors.white,
-                          child: Image.asset(
-                            AppAssets.share,
-                            width: 25,
-                            height: 25,
-                            fit: BoxFit.fill,
-                          ),
-                        )
+                        Constants.sizedBox(height: 15.0),
+                        Text(
+                          project.tools,
+                          style:
+                          AppTextStyles.normalStyle(color: Colors.black87),
+                          textAlign: TextAlign.center,
+                        ),
+
+
                       ],
                     ),
                   ),
                 ),
+                Positioned(
+                  top: 10,
+                  right: 10,
+                  child: CircleAvatar(
+                    maxRadius: 16,
+                    backgroundColor: index == hoveredIndex?Colors.white:AppColors.themeColor,
+                    child: Image.asset(
+                      AppAssets.github,
+                      width: 20,
+                      height: 20,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                )
               ],
             ),
           ),
